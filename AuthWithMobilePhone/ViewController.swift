f33ff73e7122901474b9474b4aae51cd279387d0
sergeyvorobey <phanta2408@yandex.ru>
2020-07-10 17:39:39 +0300

    Initial Commit

---
+++ AuthWithMobilePhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthWithMobilePhone
+//
+//  Created by Sergey Vorobey on 10.07.2020.
+//  Copyright © 2020 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
